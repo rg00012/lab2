6979eff4e796f49079c4a3755670643d312e792e
ryan <ryangcj07.24@ichat.sp.edu.sg>
2025-05-18 18:15:52 +0800

    added return values

--- lab2.py
+++ lab2.py
@@ -14,21 +14,26 @@ def get_user_input():
 def calc_average(numlist):
     average=sum(numlist)/len(numlist)
     print("average=" + str(average))
+    return average
 
 
 def find_min_max(numlist):
-    minmaxlist=sorted(numlist)
+    minlist=min(numlist)
+    maxlist=max(numlist)
+    minmaxlist=[minlist,maxlist]
     print("sorted list is: ",minmaxlist)
-    
+    return minmaxlist
 
 def sort_temperature(numlist):
     descendinglist=sorted(numlist,reverse=True)
     print("in descending order: ",descendinglist)
+    return descendinglist
 
 def calc_median_temperature(numlist):
     import statistics
     medianlist=statistics.median(numlist)
     print("median list=" + str(medianlist))
+    return medianlist
 
 def main():
     display_main_menu()
